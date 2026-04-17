dac1b1a41d7ad3d775f0dbb8729391a3d58cc87e
Ziad Ayoub <za5903021@gmail.com>
2026-04-17 20:33:19 +0200

    directory change

--- data.js
+++
@@ -1,6 +0,0 @@
-
-const productsData = [
-    { id: 1, name: "Vanilla Perfume", price: 500 },
-    { id: 2, name: "Crochet Ribbon", price: 150 },
-    { id: 3, name: "Handmade Bag", price: 300 }
-];
