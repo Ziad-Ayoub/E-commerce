fdade439acc2b16252026e1257783c6c383263b9
Ziad Ayoub <za5903021@gmail.com>
2026-05-02 01:35:33 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 const productsData = [
-    { id: 1, name: "Vanilla Perfume", price: 500 },
-    { id: 2, name: "Crochet Ribbon", price: 150 },
-    { id: 3, name: "Handmade Bag", price: 300 }
+    { id: 1, title: "Vanilla Perfume", price: 500 },
+    { id: 2, title: "Crochet Ribbon", price: 150 },
+    { id: 3, title: "Handmade Bag", price: 300 }
 ];
